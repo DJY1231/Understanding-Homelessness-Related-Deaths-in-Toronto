15492cd4f0caccd42421a0dc48a4ef0d106dae89
DJY1231 <156145192+DJY1231@users.noreply.github.com>
2024-01-25 02:31:53 -0500

    Create 01-download_data.R

---
+++ scripts/01-download_data.R
@@ -0,0 +1,9 @@
+#### Preamble ####
+# Purpose: Downloads and saves the data from Open Data Toronto 
+# Author: Dong Jun Yoon
+# Date: 24 January 2024
+# Contact: dongjun.yoon@mail.utoronto.ca
+
+#### Download data ####
+Need to use the csv file attached, unless download it from the original website (https://open.toronto.ca/dataset/deaths-of-people-experiencing-homelessness/).
+Cannot find for search through list_package_resources()
