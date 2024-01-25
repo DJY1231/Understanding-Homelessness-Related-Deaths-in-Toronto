1666f04c8cc44f456b218a3521dc964c04cbbdc2
DJY1231 <156145192+DJY1231@users.noreply.github.com>
2024-01-25 02:34:47 -0500

    Create 02-data_cleaning.R

---
+++ scripts/02-data_cleaning.R
@@ -0,0 +1,22 @@
+#### Preamble ####
+# Purpose: Clean any N/A in the raw plane data, and keep only the variables necessary for the paper. 
+# Author: Dong Jun Yoon
+# Date: 25 January 2024
+# Contact: dongjun.yoon@mail.utoronto.ca
+
+#### Workspace setup ####
+library(tidyverse)
+library(dplyr)
+
+#### Clean data ####
+raw_data <- read_csv("Homeless_deaths_by_demographics.csv")
+raw_data <- read_csv("Homeless_deaths_by_cause.csv")
+raw_data <- read_csv("Homeless_deaths_by_month.csv")
+
+#Replace any numerical missing value with 0. 
+data_cleaned <- raw_data
+for (col in names(data_cleaned)) {
+  if (is.numeric(data_cleaned[[col]])) {
+    data_cleaned[[col]][is.na(data_cleaned[[col]])] <- 0
+  }
+}
